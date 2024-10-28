80f3998322122c4b38dbb98d958ab5ff0c1b5a5e
Dmitry <babanov91@gmail.com>
2024-10-28 19:00:10 +0500

    smt fix

--- app/main.py
+++ app/main.py
@@ -1,12 +1,32 @@
 import uvicorn
-from fastapi import FastAPI
+from fastapi import FastAPI, Query
+from typing import Optional
+from pydantic import BaseModel
+from datetime import date
+
 
 app = FastAPI()
 
 
-@app.get("/")
-async def root():
-    return {"message": "Привет, мир"}
+@app.get("/hotels/{hotel_id}")
+async def get_hotels(
+        date_from,
+        date_to,
+        stars: Optional[int] = Query(None, ge=1, le=5),
+):
+
+    return date_from, date_to, stars
+
+
+class SBooking(BaseModel):
+    room_id: int
+    date_from: date
+    date_to: date
+
+
+@app.post("/bookings")
+async def add_booking(booking: SBooking):
+    pass
 
 if __name__ == "__main__":
     uvicorn.run("main:app", host="127.0.0.1", port=8000, reload=True)
--- readme.md
+++ readme.md
@@ -1,9 +1,12 @@
-python3 -m venv venv
-.\venv\Scripts\activate
+python3 -m venv .venv
+.\.venv\Scripts\activate
 
-pip install fastapi uvicorn
+[//]: # (422 - validation error)
+[//]: # (pip install fastapi uvicorn)
+pip install fastapi[all]
 
 uvicorn app.main:app --reload
 
 
-**set PYTHONPATH=%PYTHONPATH%;D:\education\Py\projects\fa_booking\app**
+--set PYTHONPATH=%PYTHONPATH%;D:\education\Py\projects\fa_booking\app--
+~1.5.~
